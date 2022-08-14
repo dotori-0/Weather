26134ea40a228b12e8e416130a1cb0e47f09f0e3
dotori-0 <sesan131@gmail.com>
2022-08-14 21:23:23 +0900

    [Feat] Get Current Location

---
+++ Weather/Network/Endpoint.swift
@@ -0,0 +1,19 @@
+//
+//  Endpoint.swift
+//  Weather
+//
+//  Created by SC on 2022/08/14.
+//
+
+import Foundation
+
+struct Endpoint {
+    static let currentWeatherBaseURL = "https://api.openweathermap.org/data/2.5/weather?"
+    // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+    
+    static func getCurrentWeatherURL(lat: String, lon: String) -> String {
+        let url = "\(currentWeatherBaseURL)lat=\(lat)&lon=\(lon)&appid=\(APIKey.OpenWeather)"
+        
+        return url
+    }
+}
